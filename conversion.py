b579ac0248ed600e77ba77ffbe666d4cc82b526a
MoneebaMeer <165048606+MoneebaMeer@users.noreply.github.com>
2024-05-23 20:34:45 +0100

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,32 @@
+# Cast num1 from float to integer, cast num2 from integer to float,
+# cast num3 from integer to string, cast string1 from string to integer.
+
+# declare variables
+num1 = 99.33
+num2 = 23
+num3 = 150
+string1 = "100"
+
+# cast num1 to an integer
+new_num1 = int(num1)
+print(new_num1)
+print(type(num1))
+print(type(new_num1))
+
+# cast num2 to a float
+new_num2 = float(num2)
+print(new_num2)
+print(type(num2))
+print(type(new_num2))
+
+# cast num3 to a string
+new_num3 = str(num3)
+print(new_num3)
+print(type(num3))
+print(type(new_num3))
+
+# cast string1 to an integer
+new_string1 = int(string1)
+print(new_string1)
+print(type(string1))
+print(type(new_string1))
